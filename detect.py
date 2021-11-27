aa810b2882538064b7c3610cbc62bfcea9ac511c
Mahmoud Mabrouk <mmabrouk@tu-berlin.de>
2021-11-27 13:39:59 +0100

    up

--- detect.py
+++ detect.py
@@ -100,7 +100,9 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
     if pt and device.type != 'cpu':
         model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
     dt, seen = [0.0, 0.0, 0.0], 0
-    for path, im, im0s, vid_cap, s in dataset[:600]:
+    for ii, (path, im, im0s, vid_cap, s) in enumerate(dataset):
+        if ii>600:
+            break
         t1 = time_sync()
         im = torch.from_numpy(im).to(device)
         im = im.half() if half else im.float()  # uint8 to fp16/32
